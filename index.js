b4b7f2b508d28106f20be563f63a0d616e2f7e60
Dipendra Timalsina <119526960+dipendra1186@users.noreply.github.com>
2023-02-11 22:26:24 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,2 @@
+alert("Welcome to Dipendra timalsina website!");
+console.log("i am Dipendra timalsina");
---
+++ references.js
@@ -0,0 +1,2 @@
+console.log("This is the reference part");
+alert("This is the reference page of the whole website!")
